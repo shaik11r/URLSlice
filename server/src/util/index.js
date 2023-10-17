f352a0ee224c9192fe2ba7ded737e1a1c8b3caf4
shaik11r <nadeencool11@gmail.com>
2023-10-17 12:51:11 +0530

    added getstats in utils

--- server/src/util/index.js
+++ server/src/util/index.js
@@ -20,3 +20,27 @@ export const generateShortLink=(id,domain)=>{
     const protocol=domain?"https://":"http://";
     return `${protocol}${domain||evn.DEFAULT_DOMAIN}/${id}`;
 }
+export const getInitstats=()=>{
+    return Object.create({
+        browser:{
+            chrome:0,
+            edge:0,
+            firefox:0,
+            ie:0,
+            opera:0,
+            other:0,
+            safari:0
+        },
+        os:{
+            android:0,
+            ios:0,
+            linux:0,
+            macos:0,
+            other:0,
+            windows:0,
+        },
+        country:{},
+        referrer:{},
+        city:{},
+    });
+};
