3d0a1420233efff0acbefdeedc0f7216171ac7c1
shaik11r <nadeencool11@gmail.com>
2023-10-18 13:18:21 +0530

    added createlink and getlink functionality

--- server/src/controllers/links.js
+++ server/src/controllers/links.js
@@ -4,6 +4,10 @@ const URL = require("url");
 const dns = require("node:dns");
 const { promisify } = require("util");
 const query = require("../services/link");
+const validators = require("../middleware/validators");
+const utils = require("../util/index");
+const { quartersInYear } = require("date-fns");
+const env = require("../constants/index");
 
 const dnsLookup = promisify(dns.lookup);
 const get = async (req, res) => {
@@ -13,30 +17,43 @@ const get = async (req, res) => {
   const match = {
     ...(!all && { user_id: userId }),
   };
-  const [links, total] = await Promise.all([
-    query.get(match, { limit, search, skip }),
-    query.total(match, { search }),
-  ]);
-  const data=links.map(utils.sanitize.link);
+  const [links, total] = await Promise.all([query.get(match, { limit, search, skip }), query.total(match, { search })]);
+  const data = links.map(utils.sanitize.link);
   return res.send({
     total,
     limit,
     skip,
     data,
-  })
+  });
 };
-const create=async(req,res)=>{
-  const{
-    reuse,
-    customurl,
+const create = async (req, res) => {
+  const { customurl, description, target, domain, expire_in } = req.body;
+  const domain_id = domain ? domain.id : null;
+  const targetDomain = utils.removeWww(URL.parse(target).hostname);
+  const queries = await Promise.all([
+    validators.cooldown(req.user),
+    validators.malware(req.user, target),
+    validators.linksCount(req.user),
+    customurl &&
+      query.link.find({
+        address: customurl,
+        domain_id,
+      }),
+    !customurl && utils.generateId(domain_id),
+    validators.bannedDomain(targetDomain),
+    validators.bannedHost(targetDomain),
+  ]);
+  const address = customurl || queries[4];
+  const link = await query.link.create({
+    address,
+    domain_id,
     description,
     target,
-    domain,
     expire_in,
-  }=req.body;
-  const domain_id=domain?domain.id:null;
-  const targetDomain=utils.removeWww(URL.parse(target).hostname)
-  const queries=await Promise.all([
-    validators.cooldown(req.use)
-  ])
-}
+    user_id: req.user && req.user.id,
+  });
+  if (!req.user && env.NON_USER_COOLDOWN) {
+    query.ip.add(req.realIp);
+  }
+  return res.status(201).send(utils.sanitize.link({ ...link, domain: domain?.address }));
+};
